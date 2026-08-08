bd796f748fc60b09c2c0b11d33fa3cd913a40deb
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/system_it.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>If you are missing sensible default values for your system, please leave a note on the GitHub issue tracker.</source>
+        <translation>Se mancano valori predefiniti sensati per il tuo sistema, lascia una nota nel tracker dei problemi su GitHub.</translation>
+    </message>
+    <message>
+        <source>System commands</source>
+        <translation>Comandi di sistema</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Lock</source>
+        <translation>Blocca</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Disconnetti</translation>
+    </message>
+    <message>
+        <source>Suspend</source>
+        <translation>Sospendi</translation>
+    </message>
+    <message>
+        <source>Hibernate</source>
+        <translation>Iberna</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translation>Riavvia</translation>
+    </message>
+    <message>
+        <source>Poweroff</source>
+        <translation>Spegni</translation>
+    </message>
+    <message>
+        <source>Lock the session</source>
+        <translation>Blocca la sessione</translation>
+    </message>
+    <message>
+        <source>Quit the session</source>
+        <translation>Chiudi la sessione</translation>
+    </message>
+    <message>
+        <source>Suspend to memory</source>
+        <translation>Sospendi in memoria</translation>
+    </message>
+    <message>
+        <source>Suspend to disk</source>
+        <translation>Sospendi su disco</translation>
+    </message>
+    <message>
+        <source>Restart the machine</source>
+        <translation>Riavvia la macchina</translation>
+    </message>
+    <message>
+        <source>Shut down the machine</source>
+        <translation>Spegni la macchina</translation>
+    </message>
+</context>
+</TS>
